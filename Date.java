586f6c8aec67795b33be1e67fefb52814259d8cb
Sonal <57592260+amorfati-s@users.noreply.github.com>
2021-01-29 11:10:14 -0500

    Create Date.java

---
+++ Date.java
@@ -0,0 +1,17 @@
+public class Date {
+    private int year;
+    private int month;
+    private int day;
+
+    public Date(String date) { //taking mm/dd/yyyy and create a Date object
+    }
+
+    public Date () // create object with today's date (see Calendar class)
+    {
+    }
+
+    public boolean isValid()
+    {
+        return true;
+    }
+}
