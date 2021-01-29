ed59c026e5a64879f9fc958adcd4c06725356678
Sonal <57592260+amorfati-s@users.noreply.github.com>
2021-01-29 11:10:38 -0500

    Update Library.java

--- Library.java
+++ Library.java
@@ -1,26 +1,30 @@
 public class Library {
-    private static final int CAPACITY = 4
+    private static final int CAPACITY = 4;
     private Book[] books; // array-based implementation of the bag data structure private int numBooks;
-    books = new Book[CAPAPCITY]; //allocating memory for 4 books ...COME BACK TO THIS!!
+    //books = new Book[CAPACITY]; //allocating memory for 4 books ...COME BACK TO THIS!!
     private int numBooks; // the number of books currently in the bag
 
     public Library() { //default constructor to create an empty bag
-        this(CAPACITY);
+        //initialize books capacity to 4
+        books = new Book[CAPACITY];
+        numBooks = 0;
     }
 
-    private int find(Book book) { } // helper method to find a book in the bag
+    private int find(Book book) {
+        return 0; } // helper method to find a book in the bag
     private void grow() { } // helper method to grow the capacity by 4
     public void add(Book book) { }
-    public boolean remove(Book book) { }
-    public boolean checkOut(Book book) { }
-    public boolean returns(Book book) { }
+    public boolean remove(Book book) {
+        return false;
+    }
+    public boolean checkOut(Book book) {
+        return false;
+    }
+    public boolean returns(Book book) {
+        return false;
+    }
     public void print() { } //print the list of books in the bag
     public void printByDate() { } //print the list of books by datePublished (ascending) public void printByNumber() { } //print the list of books by number (ascending)
-
-
-
-
-
 
 
 }
