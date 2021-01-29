01c38ac7e44f946b0d6f8c7ffd981deaa7a4f3b3
Sonal <57592260+amorfati-s@users.noreply.github.com>
2021-01-29 17:30:20 -0500

    created find() method

--- Library.java
+++ Library.java
@@ -4,16 +4,30 @@ public class Library {
     //books = new Book[CAPACITY]; //allocating memory for 4 books ...COME BACK TO THIS!!
     private int numBooks; // the number of books currently in the bag
 
-    public Library() { //default constructor to create an empty bag
-        //initialize books capacity to 4
+    public Library() { //default constructor to create an empty ba
         books = new Book[CAPACITY];
         numBooks = 0;
     }
 
     private int find(Book book) {
-        return 0; } // helper method to find a book in the bag
+
+        for (int i = 0; i < numBooks; i++) {
+            if (books[i].getNumber().compareTo(book.getNumber()) == 0) {
+                return 1; //1 means it was found
+            }
+            else {
+                return 0; //0 means it wasn't found
+            }
+        }
+        return 0; //false by default
+    } // helper method to find a book in the bag
+
     private void grow() { } // helper method to grow the capacity by 4
-    public void add(Book book) { }
+    
+    public void add(Book book) {
+       //TODO working on this now 
+
+    }
     public boolean remove(Book book) {
         return false;
     }
@@ -27,4 +41,4 @@ public class Library {
     public void printByDate() { } //print the list of books by datePublished (ascending) public void printByNumber() { } //print the list of books by number (ascending)
 
 
-} 
+}
