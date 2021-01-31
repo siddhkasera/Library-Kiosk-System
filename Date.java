4303c2e7c5fcd200c2eb45cd21a40710bc1b96c9
Sonal <57592260+amorfati-s@users.noreply.github.com>
2021-01-31 16:05:46 -0500

    date class setting up object (not done)

--- Date.java
+++ Date.java
@@ -1,9 +1,18 @@
+import java.util.Calendar;
+
 public class Date {
     private int year;
     private int month;
     private int day;
 
     public Date(String date) { //taking mm/dd/yyyy and create a Date object
+
+        this.setDate(day, month, year);
+
+    }
+
+    public String setDate(int day, int month, int year){
+        //Calendar.
     }
 
     public Date () // create object with today's date (see Calendar class)
@@ -15,3 +24,6 @@ public class Date {
         return true;
     }
 }
+
+
+
