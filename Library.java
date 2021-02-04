dc7e82cf5863c2082d9c96866430fbb657d6acfc
Sonal <57592260+amorfati-s@users.noreply.github.com>
2021-02-04 14:45:57 -0500

    didn't do much but i updated it library

--- Library.java
+++ Library.java
@@ -13,20 +13,9 @@ public class Library {
         numBooks = 0;
     }
 
-    private int find(Book book) {
 
-        for (int i = 0; i < numBooks; i++) {
-            if (books[i].getNumber().compareTo(book.getNumber()) == 0) {
-                return 1; //1 means it was found
-            }
-            else {
-                return 0; //0 means it wasn't found
-            }
-        }
-        return 0; //false by default
-
-    } // helper method to find a book in the bag
     private int  findIndex(Book book) {
+        System.out.println("in the find index method");
         for (int i = 0; i < numBooks; i++) {
             if (books[i].getNumber().compareTo(book.getNumber()) == 0) {
                 return i; //1 means it was found
@@ -48,15 +37,16 @@ public class Library {
         int newLength = oldLength + CAPACITY;
         Book[] book1 = new Book[newLength];
 
-        for(int i = 0; i <newLength; i++) {
+        for(int i = 0; i <oldLength; i++) {
             book1[i] = books[i];
         }
         books = book1;
     } // helper method to grow the capacity by 4
 
-//ADD and REMOVE seem to work.
+    //ADD and REMOVE seem to work.
     public void add(Book book) {
 
+        System.out.println("in the add method");
         if ((numBooks + 1) <= books.length) {
             books[numBooks] = book;
             serialNumInt++;
@@ -64,22 +54,58 @@ public class Library {
 
             //--RESOLVED -- QUESTION: how to set serial number to book? is this how it is done?
             book.setNumber(serialNum);
-           //book.setDate(); //QUESTION: is this where i should do it? I don't think it'll be good to do it here.
+            //book.setDate(); //QUESTION: is this where i should do it? I don't think it'll be good to do it here.
 
-            System.out.println(serialNum); //TESTING PURPOSES. must delete after or will lose points
+            System.out.println("the serial num is:" + serialNum); //TESTING PURPOSES. must delete after or will lose points
 
         } else {
             this.grow();
+            serialNumInt++;
+            serialNum = String.valueOf(serialNumInt);
+            book.setNumber(serialNum);
             books[numBooks] = book;
         }
 
-        System.out.println(book.getName()+" added to the Library.");
+        System.out.println(book.getName() + " added to the Library.");
+        System.out.println("the length of the books is:" + books.length);
 
         numBooks++;
-
-
+        for(int i =0; i<books.length;i++){
+            System.out.println(books[i]);
         }
 
+
+    }
+    private int find(Book book) {
+        System.out.println("in the find method");
+        int flag =0;
+        // System.out.println("the removing book number is:"+ book.getNumber());
+        for (int i = 0; i < numBooks; i++) {
+            System.out.println("inside the for loop of find method");
+            //
+            //System.out.println(books[1].getNumber());
+            System.out.println("I is:"+ i);
+            String cmp1 = books[i].getNumber();
+            System.out.println("BOOK1 IS:" + cmp1);
+            String cmp2 = book.getNumber();
+            System.out.println("BOOK2 IS:" + cmp2);
+
+            if ((books[i].getNumber()).equals(book.getNumber())){
+                System.out.println("Books found"+ books[i].getName());
+                flag = 1;
+                //return 1; //1 means it was found
+            }
+            else {
+                System.out.println("book not found");
+                //return 0; //0 means it wasn't found
+            }
+        }
+        if(flag ==1){
+            return 1;
+        }
+        return 0; //false by default
+
+    } // helper method to find a book in the bag
 
     //1 2 3 4 5
     //1 2   4 5
@@ -91,8 +117,10 @@ public class Library {
         System.out.println("remove method was called");
 
         int found = find(book);
+        // System.out.println("after finding the book in remove");
         int index = 0;
         if(found == 1) {
+            System.out.println("inside the if found loop in remove");
             index = findIndex(book);
 
             System.out.println("Book# "+ book.getNumber()+" removed.");
@@ -100,11 +128,12 @@ public class Library {
             books[index].setNumber(null);
             books[index].setDate(null);
             numBooks--;
-            //return true;
+            return true;
         }
+
         Book [] book1 = new Book[books.length];
         for(int i = index; i < books.length-1; i++) {
-            book1[i] = books[i+1];
+            book1[i] = books[i + 1];
         }
         books = book1;
         //books[books.length] //check this.
@@ -131,7 +160,7 @@ public class Library {
         return false;
     }
 
-//POSSIBLE ISSUE: this isn't returning a book back to our array I think
+    //POSSIBLE ISSUE: this isn't returning a book back to our array I think
     public boolean returns(Book book) {
 
         System.out.println("return method called");
