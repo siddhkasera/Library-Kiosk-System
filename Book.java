57e03d6d76a34027ba4d3d4592d11d179b6ff41e
Sonal <57592260+amorfati-s@users.noreply.github.com>
2021-02-05 03:49:22 -0500

    toString works. added constructors. equals method from chang and friend.

--- Book.java
+++ Book.java
@@ -20,6 +20,12 @@ public class Book{
         datePublished = new Date(date);
     }
 
+    public Book(String serial){
+        number=serial;
+        name = null;
+        checkedOut = false;
+        datePublished = null;
+    }
 
 
     public String getName(){
@@ -56,29 +62,30 @@ public class Book{
     //we can typecast an obj to our own class
     //use equals method
     @Override
-    public boolean equals(Object obj){
-        if(obj instanceof Book){
+    public boolean equals(Object obj) {
+        if (obj instanceof Book) {
             Book book = (Book) obj;
             return book.number.equals(this.number);
 
             //use the formatting in the notes. on canvas. click modules. under week #2 2.1 additional notes for project 1.-- use that
             //scroll to the bottom and write the equals method in that way.
         }
-        else {
-            return false;
-        }
+        return false;
     }
 
     @Override
     public String toString(){
 
-        //if (isCheckedOut()) {
+        if (isCheckedOut()) {
             return "Book#" + this.number + "::" + this.name + "::"+ String.valueOf(datePublished.getMonth())+"/"+String.valueOf(datePublished.getDay())
-                    +"/"+ String.valueOf(datePublished.getYear()) +"::"+ this.checkedOut;
+                    +"/"+ String.valueOf(datePublished.getYear()) +"::"+ "is checked out.";
 
-      //}
-       // String output = "Book#" + this.number + "::" + this.name + "::"+ this.datePublished +"::"+ this.checkedOut;
-      //  return output;
+        }
+        else {
+            return "Book#" + this.number + "::" + this.name + "::"+ String.valueOf(datePublished.getMonth())+"/"+String.valueOf(datePublished.getDay())
+                    +"/"+ String.valueOf(datePublished.getYear()) +"::"+ "is available.";
+            }
+
     }
 
 }
