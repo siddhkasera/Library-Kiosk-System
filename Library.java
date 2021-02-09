ae65954da9c723e5a808a6690e38284c4d424c68
Sonal <57592260+amorfati-s@users.noreply.github.com>
2021-02-09 15:16:07 -0500

    sortByDate and printByDate work now

--- Library.java
+++ Library.java
@@ -1,3 +1,5 @@
+import com.sun.jdi.IntegerValue;
+
 public class Library {
     private static final int CAPACITY = 4;
     private static final int NOT_FOUND = -1;
@@ -9,9 +11,9 @@ public class Library {
     public static String serialNum = "10000";
     public static int serialNumInt = 10000;
 
-/*
+    /*
 
-*/
+     */
     public Library() { //default constructor to create an empty bag
         //initialize books capacity to 4
         books = new Book[CAPACITY];
@@ -19,35 +21,35 @@ public class Library {
         numBooks = 0;
     }
 
-/*
+    /*
 
-*/
+     */
     private int findIndex(Book book) {
         for (int i = START; i < numBooks; i++) {
-            if (books[i].getNumber().equals(book.getNumber())){
-                return i; //1 means it was found
+            if (books[i].getNumber().equals(book.getNumber())) {
+                return i;
             }
         }
         return NOT_FOUND;
     }
 
-/*
+    /*
 
-*/
+     */
     private void grow() {
         int oldLength = books.length;
         int newLength = oldLength + CAPACITY;
         Book[] book1 = new Book[newLength];
 
-        for(int i = START; i < oldLength; i++) {
+        for (int i = START; i < oldLength; i++) {
             book1[i] = books[i];
         }
         books = book1;
     }
 
-/*
+    /*
 
-*/
+     */
     public void add(Book book) {
 
         if ((numBooks + FOUND) <= books.length) {
@@ -66,13 +68,13 @@ public class Library {
         numBooks++;
     }
 
-/*
+    /*
 
-*/
+     */
     private int find(Book book) {
         int flag = 0;
         for (int i = START; i < numBooks; i++) {
-            if(books[i].getNumber().equals(book.getNumber())){
+            if (books[i].getNumber().equals(book.getNumber())) {
                 flag = 1;
                 return flag;
             }
@@ -81,16 +83,16 @@ public class Library {
         return flag;
     }
 
-/*
+    /*
 
-*/
+     */
     public boolean remove(Book book) {
 
         int found = find(book);
         int index = START;
-        if(found == FOUND) {
+        if (found == FOUND) {
             index = findIndex(book);
-            for (int i = index; i< books.length-1;i++) {
+            for (int i = index; i < books.length - 1; i++) {
                 books[i] = books[i + 1];
             }
             numBooks--;
@@ -99,92 +101,134 @@ public class Library {
         return false;
     }
 
-/*
+    /*
 
-*/
+     */
     public boolean checkOut(Book book) {
         int found = find(book);
         int index = findIndex(book);
 
-        if(found==FOUND) {
-            if (books[index].isCheckedOut()){
+        if (found == FOUND) {
+            if (books[index].isCheckedOut()) {
                 return true;
             }
         }
-            try {
-                books[index].setCheckedOut(true);
-                return false;
-            }
-            catch (ArrayIndexOutOfBoundsException e) { //if the serial number they inputted doesn't exist in our library anymore, this is thrown
-                return true;
-            }
-     }
+        try {
+            books[index].setCheckedOut(true);
+            return false;
+        } catch (ArrayIndexOutOfBoundsException e) { //if the serial number they inputted doesn't exist in our library anymore, this is thrown
+            return true;
+        }
+    }
 
-/*
+    /*
 
-*/
+     */
     public boolean returns(Book book) {
 
         int found = find(book);
         int index = findIndex(book);
 
-        if(found==FOUND && books[index].isCheckedOut()) {
-            System.out.println("Book# "+ book.getNumber()+" return has completed.Thanks!");
+        if (found == FOUND && books[index].isCheckedOut()) {
             books[index].setCheckedOut(false);
+        }
+        if (index == -1) {
+            return true;
         }
         return false;
     }
 
+    /*
 
-/*
-
-*/
+     */
     public void print() {
         System.out.println("**List of books in library.");
-        if(numBooks == 0){
+        if (numBooks == 0) {
             System.out.println("Library catalog is empty!");
         }
-        for(int i =0; i<numBooks; i++) {
+        for (int i = 0; i < numBooks; i++) {
             System.out.println(books[i]);
         }
         System.out.println("**End of list.");
     } //print the list of books in the bag
 
+    /*
+
+     */
     public void printByDate() {
-
         System.out.println("**List of books by the date published.");
-        int month = datePublished.getMonth();
-        int day = datePublished.getDay();
-        int year = datePublished.getYear();
-
-        String date = String.valueOf(month) + "/" + String.valueOf(day) + "/" + String.valueOf(year);
-
-            if(numBooks == START){
-                System.out.println("Library catalog is empty!");
-            }
-            for(int i =START; i<numBooks; i++) {
-                //
-                System.out.println(books[i]);
-            }
-        System.out.println("**End of list.");
-        } //print the list of books by datePublished (ascending)
-
-/*    
-
-*/
-    public void printByNumber() { //print the list of books by number (ascending)
-        System.out.println("**List of books by the book numbers.");
-        if(numBooks == 0){
+        if (numBooks == 0) {
             System.out.println("Library catalog is empty!");
         }
+        sortByDate();
         for (int i = 0; i < numBooks; i++) {
-            if (Integer.parseInt(books[i].getNumber()) > Integer.parseInt(books[i + 1].getNumber())){
-                books[i] = books[i + 1];
-                System.out.println(books[i]);
+            System.out.println(books[i]);
             }
+        System.out.println("**End of list.");
+    } //print the list of books by datePublished (ascending)
+
+    /*
+    helper method for printByDate
+     */
+    public void sortByDate() {
+
+        for (int i = 0; i < numBooks-1; i++) {
+            int index = i;
+            for (int j = i +1; j < numBooks; j++){
+            if (books[i+1] == null) {
+                break;
+            }
+            if (books[j].compareByDate(books[index]) == -1) {
+                index = j;
+                //System.out.println("book1 is less than book2");
+            }
+        }
+            Book olderDate = books[index];
+            books[index] = books[i];
+            books[i] = olderDate;
+        }
+
+}
+
+    public void selectionSort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            int index = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[index]) {
+                    index = j;
+                }
+            }
+            int smallerNumber = arr[index];
+            arr[index] = arr[i];
+            arr[i] = smallerNumber;
+        }
+
+    }
+
+    public void printByNumber() { //print the list of books by number (ascending)
+        int arrSerialNumbers[] = new int[numBooks];
+        int j = 0;
+        System.out.println("**List of books by the book numbers.");
+        if (numBooks == 0) {
+            System.out.println("Library catalog is empty!");
+        }
+        //populate the serial numbers into an array
+        for (int i = 0; i < numBooks; i++) {
+
+            arrSerialNumbers[i] = (Integer.valueOf(books[i].getNumber()));
+        }
+        selectionSort(arrSerialNumbers);
+        for (int element : arrSerialNumbers) {
+            if (element == Integer.valueOf(books[j].getNumber())) {
+                System.out.println(books[j]);
+            }
+            j++;
         }
         System.out.println("**End of list.");
     }
 }
+
+
+
 
 
